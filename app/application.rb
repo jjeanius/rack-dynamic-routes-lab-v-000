98540baee327cc87960ab43daecedbeff9d58a33
Judy McGoldrick <jjeanius@verizon.net>
2018-05-29 02:19:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/item/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|i|i.name == item_name}
-      if resp.write item.price
+      if resp.write "item.price"
       else
         resp.write "Item not found"
         resp.status = 400
